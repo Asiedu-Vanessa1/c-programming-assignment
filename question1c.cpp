7553235b09435ec4d4888a04d3445d360efda6af
Asiedu-Vanessa1 <asiedunessa1@gmail.com>
2026-09-06 03:58:32 +1200

    Asiedu-Vanessa1

---
+++ question1c.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float celsius,fahrenheit;
+	
+	printf("enter temperature in celsius");
+	scanf("%f" , &celsius);
+	
+	fahrenheit = (celsius * 9/5) + 32;
+	printf("temperature in fahrenheit = %f\n" , fahrenheit);
+	
+	return 0;
+}
---
+++ question5 11.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float resistance1, resistance2, resistance3 ;
+	
+	printf(" enter resistance1 value");
+	scanf("%f" , &resistance1);
+	
+	printf("enter resistance2 value");
+	scanf("%f" , &resistance2);
+	
+	printf("enter resistance3 value");
+	scanf("%f" , &resistance3);
+	
+	if(resistance1 >= resistance2 && resistance1 >= resistance3){
+		printf("resistance1 is the largest");	
+	}
+	else if(resistance2 >= resistance1 && resistance2 >= resistance3){
+		printf("resistance2 is the largest");
+	}
+	
+	else{
+		printf("resistance3 is the largest");
+	}
+	
+	return 0;
+}
